21f27a2d430430d2d059ff15c3faaf4de5749633
doryoysterpie <ilbricconemarketing@gmail.com>
2026-03-17 23:49:52 -0400

    Update submissions/latestincyber/30-Days-of-Solidity/day13/PreorderTokens.sol
    Co-authored-by: codeant-ai[bot] <151821869+codeant-ai[bot]@users.noreply.github.com>

--- submissions/latestincyber/30-Days-of-Solidity/day13/PreorderTokens.sol
+++ submissions/latestincyber/30-Days-of-Solidity/day13/PreorderTokens.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import "./SimpleERC20.sol";
+import "../day-12/SimpleERC20.sol";
 
 contract SimplifiedTokenSale is SimpleERC20 {
     uint256 public tokenPrice;
